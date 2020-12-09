f38b91305f13b7833872cc046acd3f271411bbf0
shoblik <shoblik@yahoo.com>
2020-12-08 19:25:01 -0800

    layed out the ground work to start building out the site

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<?php require('./views/templates/head.php'); ?>
+<body>
+<div class="page-container">
+    <?php require('./views/templates/nav.php'); ?>
+
+</div>
+<?php require('./views/templates/footer.php'); ?>
+</body>
+</html>
---
+++ public/views/templates/head.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Web development services and hosting">
+    <link rel="stylesheet" href="./css/style.css">
+    <script defer src="./js/swup.min.js"></script>
+    <script defer src="./js/main.js"></script>
+    <!--[if lt IE 9]>
+    <script defer src="./js/ieFix.js"></script>
+    <![endif]-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hackberry</title>
+</head>
